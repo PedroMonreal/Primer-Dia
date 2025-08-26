b56aaec533e9825e87ba42865fd0e7a07c481918
PedroMonreal <pedromonrealhernandez@gmail.com>
2025-08-26 13:40:24 -0600

    Se agregó número de control

--- app/src/main/java/com/example/primerdia/MainActivity.kt
+++ app/src/main/java/com/example/primerdia/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PrimerDiaTheme {
                 //Pedro
+                //22130532
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
